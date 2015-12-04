c9a30da048bdf08bb808aa741babce1beb353e04
Benson Wong <mostlygeek+git@gmail.com>
2015-12-04 14:15:47 -0800

    add server

---
+++ i-print-envs.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		for _, env := range os.Environ() {
+			fmt.Fprintf(w, "%s\n", env)
+		}
+	})
+
+	var port string
+
+	p := os.Getenv("PORT")
+	if p == "" {
+		log.Fatal("PORT environment variable required")
+	} else {
+		portNum, err := strconv.Atoi(p)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if portNum < 1 || portNum > 65535 {
+			log.Fatal("Invalid Port Range")
+		}
+
+		port = strconv.Itoa(portNum)
+	}
+
+	fmt.Printf("Listening on :%s\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
